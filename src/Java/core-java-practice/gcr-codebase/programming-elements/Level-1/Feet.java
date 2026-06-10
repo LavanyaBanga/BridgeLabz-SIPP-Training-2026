4c2d456fe6bcc32563d005336e1a7ffc81a03174
Lavanya Banga <lavanyabanga14@gmail.com>
2026-06-10 19:00:37 +0530

    assignment

---
+++ src/Java/core-java-practice/gcr-codebase/programming-elements/Level-1/Feet.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Feet{
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter distance in feet: ");
+        double distanceInFeet = input.nextDouble();
+
+        double yards = distanceInFeet / 3;
+        double miles = yards / 1760;
+
+        System.out.println("Distance in feet = " + distanceInFeet);
+        System.out.println("Distance in yards = " + yards);
+        System.out.println("Distance in miles = " + miles);
+
+        input.close();
+    }
+}
