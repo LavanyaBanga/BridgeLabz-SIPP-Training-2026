392b3e643072ddbb297d4dc28b3fd87434f86fdf
Lavanya Banga <lavanyabanga14@gmail.com>
2026-06-15 12:45:50 +0530

    this is java

---
+++ src/Java/core-java-practice/gcr-codebase/Arrays/Level-1/mean.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class mean {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double[] heights = new double[11];
+        double sum = 0;
+
+        System.out.println("Enter heights of 11 football players:");
+
+        for (int i = 0; i < heights.length; i++) {
+            heights[i] = sc.nextDouble();
+            sum += heights[i];
+        }
+
+        double mean = sum / 11;
+
+        System.out.println("Mean Height of Football Team = " + mean);
+
+        sc.close();
+    }
+}
