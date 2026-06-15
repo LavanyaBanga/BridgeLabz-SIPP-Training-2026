a78a0f01ed98a8df32ce68d3c8e2f34491c2a866
Lavanya Banga <lavanyabanga14@gmail.com>
2026-06-15 13:18:25 +0530

    this is java

---
+++ src/Java/core-java-practice/gcr-codebase/String/Level-1/ArrayIndexDemo.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class ArrayIndexDemo {
+
+    public static void generateException(String[] names) {
+        System.out.println(names[names.length]);
+    }
+
+    public static void handleException(String[] names) {
+
+        try {
+
+            System.out.println(names[names.length]);
+
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("ArrayIndexOutOfBoundsException Handled");
+        }
+        catch (RuntimeException e) {
+            System.out.println("RuntimeException Handled");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String[] names = new String[3];
+
+        System.out.println("Enter 3 Names:");
+
+        for (int i = 0; i < names.length; i++) {
+            names[i] = sc.next();
+        }
+
+        // generateException(names);
+
+        handleException(names);
+
+        sc.close();
+    }
+}
---
+++ src/Java/core-java-practice/gcr-codebase/String/Level-1/NullPointerDemo.java
@@ -0,0 +1,30 @@
+public class NullPointerDemo {
+
+    public static void generateException() {
+
+        String text = null;
+
+        System.out.println(text.length());
+    }
+
+    public static void handleException() {
+
+        try {
+
+            String text = null;
+
+            System.out.println(text.length());
+
+        } catch (NullPointerException e) {
+
+            System.out.println(
+                    "NullPointerException Handled");
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+        handleException();
+    }
+}
