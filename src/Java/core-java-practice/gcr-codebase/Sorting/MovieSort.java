8f3514779f7ad7ae107406ea8ac2440f5762b81d
Lavanya Banga <166047096+LavanyaBanga@users.noreply.github.com>
2026-07-07 12:40:39 +0530

    Create MovieSort.java

---
+++ src/Java/core-java-practice/gcr-codebase/Sorting/MovieSort.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class MovieRatingsSelectionSort {
+
+    public static void selectionSort(int[] ratings) {
+        int n = ratings.length;
+
+        for (int i = 0; i < n - 1; i++) {
+
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (ratings[j] < ratings[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = ratings[i];
+            ratings[i] = ratings[minIndex];
+            ratings[minIndex] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] ratings = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            ratings[i] = sc.nextInt();
+        }
+
+        selectionSort(ratings);
+
+        for (int x : ratings) {
+            System.out.print(x + " ");
+        }
+    }
+}
