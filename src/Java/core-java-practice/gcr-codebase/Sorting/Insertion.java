8dee59939601b32fd95336223f722ed5540c9dcb
Lavanya Banga <166047096+LavanyaBanga@users.noreply.github.com>
2026-07-07 12:38:41 +0530

    Create Insertion.java

---
+++ src/Java/core-java-practice/gcr-codebase/Sorting/Insertion.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class InsertionSortExample {
+
+    static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] ids = {12, 11, 13, 5, 6};
+
+        insertionSort(ids);
+
+        System.out.println(Arrays.toString(ids));
+    }
+}
