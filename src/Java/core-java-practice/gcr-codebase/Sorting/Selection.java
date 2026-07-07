e93d0a9f52deaa87d4ea63d1a715846603c53add
Lavanya Banga <166047096+LavanyaBanga@users.noreply.github.com>
2026-07-07 12:38:59 +0530

    Create Selection.java

---
+++ src/Java/core-java-practice/gcr-codebase/Sorting/Selection.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class SelectionSortExample {
+
+    static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] ratings = {8, 5, 9, 7, 6, 10};
+
+        selectionSort(ratings);
+
+        System.out.println(Arrays.toString(ratings));
+    }
+}
