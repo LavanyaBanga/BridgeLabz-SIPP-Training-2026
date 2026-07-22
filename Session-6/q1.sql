cee4e81248a68084c2b196317b8e98b3be1d888e
Lavanya Banga <166047096+LavanyaBanga@users.noreply.github.com>
2026-07-22 12:26:41 +0530

    Create q1.sql

---
+++ Session-6/q1.sql
@@ -0,0 +1,21 @@
+covid_cases(
+    Country,
+    Date,
+    Confirmed_Cases,
+    Recoveries,
+    Population,
+    infection_rate
+);
+
+covid_deaths(
+    Country,
+    Date,
+    Deaths
+);
+
+covid_vaccines(
+    Country,
+    Date,
+    Doses_Distributed,
+    Doses_Administered
+);
